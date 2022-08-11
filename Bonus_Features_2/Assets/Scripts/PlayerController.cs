99001b24310c7065ac7586fa211fa6a3534f706d
Elad Chamilevsky <elad.ch.631@gmail.com>
2022-08-11 16:07:12 +0300

    Easy: Vertical player movement

--- Bonus_Features_2/Assets/Scripts/PlayerController.cs
+++ Bonus_Features_2/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject projectilePrefab;
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 15.0f;
+    public float maxZ = 15.0f;
+    public float minZ = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +22,30 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Keep the player in bounds
+        // enforce x axis limits
         if (transform.position.x < -xRange) {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if (transform.position.x > xRange) {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        
+        // enforce z axis limits
+        if (transform.position.z < minZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, minZ);
+        }
+        if (transform.position.z > maxZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, maxZ);
+        }
+
         // Move the player to the left\right
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // Move the player forward\backward
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             // Launch projectile from the player
