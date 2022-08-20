89a2baf21be281e95d9665ec3e7a9662d1171394
Elad Chamilevsky <elad.ch.631@gmail.com>
2022-08-20 21:20:09 +0300

    create a lookDirection variable

--- Prototypes/Prototype_4/Assets/Course Library/Scripts/Enemy.cs
+++ Prototypes/Prototype_4/Assets/Course Library/Scripts/Enemy.cs
@@ -18,6 +18,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
     }
 }
