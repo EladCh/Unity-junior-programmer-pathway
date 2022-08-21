7dc1b63f84d2012ececfdf238503ed2f1e252b0d
Elad Chamilevsky <elad.ch.631@gmail.com>
2022-08-21 17:54:19 +0300

    Destroy powerup on collision

--- Prototypes/Prototype_4/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototypes/Prototype_4/Assets/Course Library/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerup = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +21,14 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
     }
 }
