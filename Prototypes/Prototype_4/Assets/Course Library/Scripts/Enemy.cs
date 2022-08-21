ff13a2909256e3a06a4f21ebb6bf1c8b60c23aa3
Elad Chamilevsky <elad.ch.631@gmail.com>
2022-08-21 18:36:30 +0300

    Spawn enemies in increasing wave num and destory if fall off

--- Prototypes/Prototype_4/Assets/Course Library/Scripts/Enemy.cs
+++ Prototypes/Prototype_4/Assets/Course Library/Scripts/Enemy.cs
@@ -20,5 +20,10 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -1)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Prototypes/Prototype_4/Assets/Course Library/Scripts/SpawnManager.cs
+++ Prototypes/Prototype_4/Assets/Course Library/Scripts/SpawnManager.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrefab;
     private float spwanRange = 9.0f;
+    public GameObject enemyPrefab;
+    public int enemyCount;
+    public int waveNumber = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
