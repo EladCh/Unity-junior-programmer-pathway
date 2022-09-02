b0cbcd0af56675667a70445ecaa2bb9d22d32ef5
Elad Chamilevsky <elad.ch.631@gmail.com>
2022-09-02 10:53:23 +0300

    Bonus features 5 (#20)
    * initial
    
    * Easy: add Lives UI
    
    * Medium: add Music volume
    
    * Hard: add Pause ability
    
    * Expert: Click-and-swipe
    
    * Update readme.md

---
+++ Bonus_Features/Bonus_Features_5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gamOverText;
+    public TextMeshProUGUI livesText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public GameObject pauseScreen;
+    public bool isGameActive;
+    private int score;
+    private int lives;
+    private float spawnRate = 1.0f;
+    private bool paused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if the user pressed the p key
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gamOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        UpdateLives(3);
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        } else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
---
+++ Bonus_Features/Bonus_Features_5/readme.md
@@ -0,0 +1,9 @@
+![New video](https://user-images.githubusercontent.com/29371222/188086527-16006a81-8fde-4685-b2e5-507036bfc9f4.gif)
+
+This tutorial outlines four potential bonus features for the Quick Click Prototype at varying levels of difficulty: 
+* Easy: Lives UI
+* Medium: Music volume
+* Hard: Pause menu
+* Expert: Click-and-swipe
+
+to see the implementation of each difficulty level please see the commits of https://github.com/EladCh/Unity-junior-programmer-pathway/pull/20
