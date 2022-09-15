0eaabe426c28dec1dfdf506b719fb5a8e85f4c3c
Elad Chamilevsky <elad.ch.631@gmail.com>
2022-09-15 16:14:45 +0300

    done

--- Challenges/ErrorProject/Assets/Scripts/CongratScript.cs
+++ Challenges/ErrorProject/Assets/Scripts/CongratScript.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CongratScript : MonoBehaviour
 {
@@ -10,6 +12,7 @@ public class CongratScript : MonoBehaviour
     
     private float RotatingSpeed;
     private float TimeToNextText;
+    [SerializeField] private Vector3 centerOfMass;
 
     private int CurrentText;
     
@@ -17,21 +20,26 @@ public class CongratScript : MonoBehaviour
     void Start()
     {
         TimeToNextText = 0.0f;
-        CurrentText = 0
+        CurrentText = 0;
         
-        RotatingSpeed = 1.0;
+        RotatingSpeed = 0.5f;
 
-        TextToDisplay.Add("Congratulation");
-        TextToDisplay.Add("All Errors Fixed");
+        TextToDisplay = new List<string>
+        {
+            "Congratulation",
+            "All Errors Fixed"
+        };
 
         Text.text = TextToDisplay[0];
-        
+
         SparksParticles.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Text.gameObject.transform.RotateAround(centerOfMass, Vector3.up, RotatingSpeed);
+
         TimeToNextText += Time.deltaTime;
 
         if (TimeToNextText > 1.5f)
@@ -42,7 +50,7 @@ public class CongratScript : MonoBehaviour
             if (CurrentText >= TextToDisplay.Count)
             {
                 CurrentText = 0;
-
+            }
 
             Text.text = TextToDisplay[CurrentText];
         }
