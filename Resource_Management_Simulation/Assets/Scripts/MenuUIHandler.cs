cb1390100648ed5f5f4a3f5d967daee22a6c1dbb
Elad Chamilevsky <elad.ch.631@gmail.com>
2022-09-16 10:41:11 +0300

    scene flow

--- Resource_Management_Simulation/Assets/Scripts/MenuUIHandler.cs
+++ Resource_Management_Simulation/Assets/Scripts/MenuUIHandler.cs
@@ -2,6 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -20,5 +25,19 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
--- Resource_Management_Simulation/Assets/Scripts/UI/UIMainScene.cs
+++ Resource_Management_Simulation/Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
